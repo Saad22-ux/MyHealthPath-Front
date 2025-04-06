d690434fb372d9eb7f403b5f87f5826c6ac9acb3
lyazidiali <lyazidiali041@gmail.com>
2025-04-06 19:23:34 +0100

    css style

--- MyHealthPath-Front/src/app/login/login.component.ts
+++ MyHealthPath-Front/src/app/login/login.component.ts
@@ -10,6 +10,7 @@ import { AuthService } from '../auth.service';
   imports: [CommonModule, FormsModule],
   templateUrl: './login.component.html',
   styleUrls : ['./login.component.css']
+  
 })
 export class LoginComponent {
   @Input() email = '';
